f49d71ff0180d3e354d119c10d6cce80653076f3
Ali Asar Khowaja <khowajaaliasar@gmail.com>
2023-09-25 14:08:07 +0500

    Added Splash Controller.

---
+++ lib/constants/constants.dart
@@ -0,0 +1,9 @@
+library constants;
+
+import 'package:flutter/material.dart';
+
+part 'assets.dart';
+part 'borders.dart';
+part 'colors.dart';
+part 'sizes.dart';
+part 'strings.dart';
---
+++ lib/constants/strings.dart
@@ -0,0 +1,90 @@
+// ignore_for_file: constant_identifier_names
+
+part of constants;
+
+class AppStrings {
+  // strings
+  static const String APP_NAME = "AAGSA";
+  static const String APP_FULL_NAME = "AL-AZHAR GARDEN";
+  static const String APP_FULL_NAME_1 = "STUDENT’S ASSOCIATION";
+
+  static const String COPYRIGHTS =
+      "Copyright © AAGSA 2023. All Rights Reserved.";
+
+  static const String LOG_IN = "Login";
+  static const String LOG_IN_2 = "LOGIN";
+  static const String LOG_IN_EX = "SECURED LOGIN";
+  static const String LOG_OUT = "Logout";
+  static const String LOG_OUT_2 = "LOGOUT";
+
+  static const String FORGOT_PASSWORD = "Forgot Password?";
+  static const String FORGOT_PASSWORD_2 = "FORGOT PASSWORD ?";
+  static const String RESET_PASSWORD = "RESET PASSWORD";
+
+  // hint_text
+  static const String FIRST_NAME = "First Name";
+  static const String USER_NAME = "Username";
+  static const String USER_NAME_2 = "USER NAME";
+  static const String LAST_NAME = "Last Name";
+  static const String NAME = "Name";
+  static const String EMAIL_ADDRESS = "Email Address";
+  static const String EMAIL = "Email";
+  static const String EMAIL_2 = "EMAIL";
+  static const String PASSWORD = "Password";
+  static const String PASSWORD_2 = "PASSWORD";
+
+  static const String SEARCH = "Search";
+  static const String SEARCH_HINT_TEXT = "Search here ..";
+  static const String FILTER = "Filter";
+
+  static const String UPLOAD_IMAGE = "Upload Image";
+  static const String CAPTURE_OR_UPLOAD_IMAGE =
+      "Take photo from Camera or upload image from Gallery";
+  static const String CAPTURE_FROM_CAMERA = "Take Photo from Camera";
+  static const String UPLOAD_FROM_GALLERY = "Upload Photo from Gallery";
+
+  // Toggles
+  static const String TAXABLE = "Taxable";
+
+  // Buttons
+
+  static const String BACK = "Back";
+  static const String DONE = "Done";
+  static const String SUBMIT = "Submit";
+  static const String CANCEL = "Cancel";
+  static const String SAVE = "Save";
+  static const String NEXT = "Next";
+  static const String PREVIOUS = "Previous";
+
+  // Api Message Strings
+  static const String LOADING = 'Loading';
+  static const String NO_INTERNET_CONNECTION = 'No internet connection';
+  static const String SERVER_NOT_RESPONDING = 'Server not responding';
+  static const String SOMETHING_WENT_WRONG = 'Something went wrong';
+  static const String API_NOT_FOUND = 'Api not found';
+  static const String SERVER_ERROR = 'Server error';
+
+  // Exception Messages
+  static const String TRY_AGAIN = 'Try Again';
+  static const String TOO_MUCH_FILTERING = 'Too much filtering';
+  static const String NO_MATCH_FOUND =
+      'We couldn\'t find any results matching your applied filters.';
+  static const String TRY_AGAIN_LATER =
+      'The application has encountered an unknown error.\n'
+      'Please try again later.';
+  static const String CONNECTION_TRY_AGAIN =
+      'Please check internet connection and try again.';
+
+  // Get Storage Keys
+  static const String THEME_BOX_KEY = 'userThemeStore';
+  static const String THEME_MODE_KEY = 'isDarkMode';
+  static const String THEME_ACCENT_KEY = 'accentColor';
+
+  // Font Names
+  static const String MONTSERRAT = 'Montserrat';
+  static const String POPPINS = 'Poppins';
+  static const String WORK_SANS = 'Work Sans';
+
+  // Get Nested Navigation Keys
+  static const int GET_NESTED_AUTH_KEY = 1;
+}
